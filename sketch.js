8941f10fa29728a26b30a749869e50c973c60a93
Glitch (hello-webpage) <none>
2021-06-03 02:06:23 +0000

    🌟🚔 Checkpoint ./sketch.js:3856783/12759 ./index.html:3856783/14

--- sketch.js
+++ sketch.js
@@ -1,9 +1,45 @@
-function setup() {
-  createCanvas(600, 500);
+let logo
+let dvdImage
+
+function setup(){
+  createCanvas(800, 600);
+  // Load the image once.
+  dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
+
+  // Set up an object with starting values.
+  logo = {
+    x: 50,
+    y: 50,
+    xVelocity: 1,
+    yVelocity: 1,
+    width: 200,
+    height: 150
+  }
 }
 
-function draw() {
-  background(230);
-  // Delete the slashes on the next line to "uncomment" the ellipse
-  // ellipse(50, 50, 50, 50)
+function draw(){
+  background(220);
+
+  // Check to make sure the image isn't at or over the edge of the screen for horizontal movement.
+  if (logo.x > width - logo.width) {
+    // If it's too far right, make velocity negative
+    logo.xVelocity = -1 * logo.xVelocity;
+  } else if (logo.x < 0) {
+    // if it's too far left, make the velocity positive
+    logo.xVelocity = -1 * logo.xVelocity;
+  }
+
+  // Same check, but for vertical movement. Reverse it in either case.
+  if (logo.y > height - logo.height) {
+    logo.yVelocity = -1 * logo.yVelocity;
+  } else if (logo.y < 0) {
+    logo.yVelocity = -1 * logo.yVelocity;
+  }
+
+  // Move the shape by changing the values of x and y
+  logo.x += logo.xVelocity;
+  logo.y += logo.yVelocity;
+
+  // Draw the logo at the new position.
+  image(dvdImage, logo.x, logo.y, logo.width, logo.height);
 }
