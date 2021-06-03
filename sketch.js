0e7ad65f7407cbb47fd9cdbe3abf7e489b9bcde7
Glitch (hello-webpage) <none>
2021-06-03 03:08:38 +0000

    🎭🐷 Checkpoint ./sketch.js:3856783/1104

--- sketch.js
+++ sketch.js
@@ -26,34 +26,21 @@ function draw(){
   background(230, 90, 50, .8);
   drawBubbles();
 
-  // Check to make sure the image isn't at or over the edge of the screen for horizontal movement.
-  if (logo.x > width - logo.width) {
-    // If it's too far right, make velocity negative
-    logo.xVelocity = -1 * logo.xVelocity;
-  } else if (logo.x < 0) {
-    // if it's too far left, make the velocity positive
-    logo.xVelocity = -1 * logo.xVelocity;
-  }
+  
 
-  // Same check, but for vertical movement. Reverse it in either case.
-  if (logo.y > height - logo.height) {
-    logo.yVelocity = -1 * logo.yVelocity;
-  } else if (logo.y < 0) {
-    logo.yVelocity = -1 * logo.yVelocity;
-  }
-
-  // Move the shape by changing the values of x and y
-  logo.x += logo.xVelocity;
-  logo.y += logo.yVelocity;
-
-  // Draw the logo at the new position.
-  image(dvdImage, logo.x, logo.y, logo.width, logo.height);
 }
 
-function drawBubble() {
-  for (let i = 0; i < 500; i++){
+function drawBubbles() {
+  for (let i = 0; i < 200; i++){
     noStroke();
-    fill(369, randin(90,100), 50, .8)
-    ellipse(random(width), random(height), random(1,5))
+    fill(random(200,229), random(95,100), 50, .8)
+    ellipse(random(width), random(height), random(10,16))
+  }
+}
+
+function createBubbles() {
+  for (let i = 0; i < 200; i++){
+    let color = fill(random(200,229), random(95,100), 50, .8)
+    ellipse(random(width), random(height), random(20,25))
   }
 }
