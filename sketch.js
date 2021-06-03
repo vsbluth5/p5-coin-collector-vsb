3cde56ec252a6b55919cc7f322d6601e9ec47de6
Glitch (hello-webpage) <none>
2021-06-03 03:20:36 +0000

    🎽🎊 Checkpoint ./sketch.js:3856783/682

--- sketch.js
+++ sketch.js
@@ -1,46 +1,38 @@
 let logo
-let dvdImage
-let fish;
-let bubbles;
-let seaweed;
-let sunbeams;
-let food;
+let fish = [];
+let bubbles = [];
+let seaweed=[];
+let sunbeams = []
+let food = [];
 
 
 function setup(){
   colorMode(HSB, 360, 100, 100, 1)
   createCanvas(1200, 800);
- 
-  // Set up an object with starting values.
-  logo = {
-    x: 50,
-    y: 50,
-    xVelocity: 1,
-    yVelocity: 1,
-    width: 200,
-    height: 150
-  }
+  createBubbles();
+
 }
 
 function draw(){
   background(230, 90, 50, .8);
   drawBubbles();
 
-  
-
 }
 
 function drawBubbles() {
-  for (let i = 0; i < 200; i++){
+  for (let i = 0; i < bubbles.length; i++){
     noStroke();
     fill(random(200,229), random(95,100), 50, .8)
-    ellipse(random(width), random(height), random(10,16))
+    ellipse(bubbles[i].x, bubbles[i].y, bubbles[i].diam)
   }
 }
 
 function createBubbles() {
   for (let i = 0; i < 200; i++){
-    let color = fill(random(200,229), random(95,100), 50, .8)
-    ellipse(random(width), random(height), random(20,25))
+    let hue = random(200,230)
+    let xPos = random(width)
+    let yPos = random(height)
+    let size = random(20,25)
+    bubbles[i] = {x: xPos, y: yPos, diam: size}
   }
 }
