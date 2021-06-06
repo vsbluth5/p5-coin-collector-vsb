6c3dbd5ca397c36a77abd038ae6f3e099755e1ea
Glitch (hello-webpage) <none>
2021-06-06 00:22:57 +0000

    🔋🎡 Checkpoint ./sketch.js:3856783/718

--- sketch.js
+++ sketch.js
@@ -2,11 +2,13 @@ let logo
 let fish = [];
 let bubbles = [];
 let poppingBubbles = [];
-
+let numPops;
 
 function setup(){
   colorMode(HSB, 360, 100, 100, 1)
   createCanvas(1200, 800);
+  numPops = 3;
+  createPoppingBubbles();
   createBubbles();
 
 }
@@ -14,6 +16,7 @@ function setup(){
 function draw(){
   background(230, 90, 50, .8);
   drawBubbles();
+  drawPoppingBubbles();
 
 }
 
@@ -35,4 +38,23 @@ function createBubbles() {
     let size = random(5,30)
     bubbles[i] = {x: xPos, y: yPos, diam: size, hue: h}
   }
+  
+  function drawPoppingBubbles() {
+  for (let i = 0; i < poppingubbles.length; i++){
+    fill(poppinhBubbles[i].hue, random(10,50), 50, .9)
+    bubbles[i].y--;
+    if (poppingubbles[i].y <= 0) bubbles[i].y = height;
+    ellipse(poppingBubbles[i].x, poppingBubbles[i].y, poppingBubbles[i].diam)
+  }
+}
+
+function createPoppingBubbles() {
+  for (let i = 0; i < numPops; i++){
+    let h = random(50,75)
+    let xPos = random(width)
+    let yPos = random(height)
+    let size = random(25,40)
+    poppingBubbles[i] = {x: xPos, y: yPos, diam: size, hue: h}
+  }
+}
 }
